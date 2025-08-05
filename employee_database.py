bed1b317c5e1f30853fbffa569042ebb220b1086
Saqib528 <saqibmanzoor@gmail.com>
2025-08-05 16:47:37 +0500

    new repo employee database

---
+++ employee_database.py
@@ -0,0 +1,48 @@
+import sqlite3
+
+conn= sqlite3.connect('employee.db')
+
+cur = conn.cursor()
+
+cur.execute("""
+            
+        CREATE TABLE IF NOT EXISTS employee_id (
+
+        id INTEGER PRIMARY KEY AUTOINCREMENT,         
+        name TEXT ,
+        age INTEGER,
+        e_mail TEXT,
+        department TEXT
+         
+            )
+
+""")
+
+employee_id = [
+   
+       ('Ahmed ',24,'ahmed33@gmail.com','Assistant Manager'),
+       ('Usama',27,'usama_mir@gmail.com','Accountant'),
+       ('Danish',29,'danish_ali@gmail.com','HR')
+
+
+    
+]
+
+cur.executemany('INSERT INTO employee_id(name,age,e_mail,department)VALUES(?,?,?,?)',employee_id)
+
+conn.commit()
+
+cur.execute('SELECT*FROM employee_id')
+
+row = cur.fetchall()
+
+print("TABLE CONTENT")
+for rows in row:
+    print(rows)
+
+conn.close()
+
+import os
+
+if os.path.exists('employee.db'):
+    os.remove('employee.db')
