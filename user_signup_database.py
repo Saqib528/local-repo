707672b90795c2c2f8a1c382ed11d5b1a675f757
Saqib528 <saqibmanzoor@gmail.com>
2025-08-06 16:43:54 +0500

    user sign_up datbase

---
+++ user_signup_database.py
@@ -0,0 +1,45 @@
+import sqlite3
+
+conn=sqlite3.connect('sign_up.db')
+
+cur=conn.cursor()
+
+cur.execute("""CREATE TABLE IF NOT EXISTS user(
+            
+           first_name TEXT,
+           last_name TEXT,
+            date_of_birth INTEGER,
+            gender TEXT,
+            e_mail TEXT
+            
+            
+            )
+""")
+
+user=[
+    ('Ali','Hassan',2001,'Male','hassanali22@gmail.com'),
+    ('Umar','Ahmed',1995,'male','umar_ali19@gmail.com'),
+    ('Muhammad','Waqar',2003,'Male','muhammad_waqar222@gmail.com'),
+    ('Ayesha','Khan',2000,'Female','ayesha_khan@gmail.com'),
+    ('Sadia','Hassan',2005,'Female','sadia_hassan121@gmail.com'),
+    ('Maryam','Ahmed',2002,'Female','maryam1ahmed@gmail.com'),
+    ('Salman','Nazir',1999,'Male','salman_nazir333@gmail.com')
+
+]
+
+cur.executemany("INSERT INTO user (first_name,last_name,date_of_birth,gender,e_mail)VALUES(?,?,?,?,?)",user)
+
+conn.commit()
+
+cur.execute("SELECT *FROM user")
+
+row=cur.fetchall()
+
+for rows in row:
+    print(rows)
+
+conn.close()
+
+import os 
+if os.path.exists('sign_up.db'):
+    os.remove('sign_up.db')
